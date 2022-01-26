0d6dd53b6f7f76f0f69fbb856c23d8b551398777
liamicious <79270208+liamicious@users.noreply.github.com>
2022-01-26 16:41:07 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+// import form group
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Todo } from './todo';
+
+// import uuid
+import { v4 as uuidv4 } from 'uuid';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +14,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TFIP_Day23';
+  // this method requires it to be initialised in a constructor as compared to the slides (onInit)
+  // retrieving data is always on Init, never in constructor
+  form: FormGroup;
+
+  // declare/initialise the respective FormControls
+  taskFormControl = new FormControl('', [Validators.required])
+  priorityFormControl = new FormControl('', [Validators.required])
+  dueDateFormControl = new FormControl('', [Validators.required])
+  tomorrow = new Date();
+  todosValues: Todo[]= [];
+  priorities = ["Low", "Medium", "High", "Urgent!"];
+
+  constructor(private fb: FormBuilder){
+    this.tomorrow.setDate(this.tomorrow.getDate()+1);
+    this.form = this.fb.group({
+      task: this.taskFormControl,
+      priority: this.priorityFormControl,
+      dueDate: this.dueDateFormControl
+    })
+  }
+
+  addTodo() {
+    console.log("add todo: ");
+    // call the uuid function
+    let taskId = uuidv4();
+    let singleTodo = new Todo(
+      this.form.value.task,
+      this.form.value.priority,
+      this.form.value.dueDate,
+      taskId
+    )
+
+    this.todosValues.push(singleTodo),
+    this.taskFormControl.reset(),
+    this.priorityFormControl.reset(),
+    this.dueDateFormControl.reset(),
+    localStorage.setItem(taskId, JSON.stringify(singleTodo))
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+// import MaterialModule and Flex Layout
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// import material moment adapter stuff
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter, MAT_MOMENT_DATE_ADAPTER_OPTIONS } from '@angular/material-moment-adapter';
+// import additional libraries from material/core
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,9 +23,28 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-SG' },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [ MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
